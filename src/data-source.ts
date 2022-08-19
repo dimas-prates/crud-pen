d984915e326b5c6337aabd5960165d1896459548
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-19 05:55:26 -0300

    :sparkles: feat(data-source.ts): Updated file with environment variables + migration/entity location

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
+import 'dotenv/config'
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "dockervm",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "julius",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as number | undefined,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+    entities: [`${__dirname}/**/entity/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migration/*.{ts,js}`],
+    // subscribers: [],
 })
