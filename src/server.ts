b94bcb2859398704a07d17ea6b7dd7842a102fe8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-19 11:46:39 -0300

    :ok_hand: styles(server.ts): Formatted the file

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import { AppDataSource } from './data-source'
 import app from "./app"
 const appPort = process.env.PORT || 3000
 
-AppDataSource.initialize().then(() => {
-    const server = app.listen(appPort, () => { console.log(`Server's running on port: ${appPort}`) })
-
+AppDataSource.initialize().then((result) => {
+    const server = app.listen(appPort, () => {
+        console.log(`Server's running on port: ${appPort}`)
+    })
     process.on('SIGINT', () => {
         server.close()
         console.log(`Server has stopped`)
