10d8ad579e4a6f62fb14c424ac15929db44c69ba
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-19 11:47:39 -0300

    :sparkles: feat(route - user.ts): adjusted delete route + add new route for the user's balance

--- src/route/user.ts
+++ src/route/user.ts
@@ -5,8 +5,9 @@ const userRoutes = Router()
 
 userRoutes.get('/', new UserController().list)
 userRoutes.get('/:id', new UserController().get)
+userRoutes.get('/balance/:id', new UserController().getBalance)
 userRoutes.post('/', new UserController().create)
 userRoutes.put('/:id', new UserController().update)
-userRoutes.delete('/:id', new UserController().remove)
+userRoutes.delete('/:id', new UserController().delete)
 
 export default userRoutes;
