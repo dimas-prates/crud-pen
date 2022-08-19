cfb974bc1bf63b51d50638d51a7f0d9e87d6318d
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-19 11:43:57 -0300

    :sparkles: feat(app.ts): Add new route, balance's routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import userRoutes from './route/user'
+import balanceRoutes from './route/balance'
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use('/user', userRoutes)
+app.use('/balance', balanceRoutes)
 // app.use('/', (req, res) => {
 //     const { method, hostname, baseUrl } = req
 //     return res.send(`remove(): Method ${method} on ${hostname}${baseUrl}`)
